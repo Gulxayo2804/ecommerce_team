133010f481c33dcd27e12f5ce37065ea142bb82f
Maxbuba Salimova <maxbubacoc@gmail.com>
2021-07-24 00:46:23 +0500

    product

--- models/Products.js
+++ models/Products.js
@@ -10,3 +10,45 @@ best_seller ->number -> default >0
 delverTime->
 image[]
 */
+
+const mongoose = require ('mongoose')
+const productSchema = new  mongoose.Schema({
+    name : 
+        {
+            uz: { type : String , required: true } ,
+            ru: { type : String , required: true } ,
+        } ,
+    CategoryID: 
+        { 
+            type : mongoose.Schema.Object.ID , ref : "Catgeory",required: true  
+        } ,
+    price : 
+        { 
+            type : Number , required: true 
+        } ,
+    color :
+        [{
+            type : String , required: true
+        }] ,
+    description : 
+        { 
+            type : String , required: true 
+        } ,
+    best_seller : 
+        { 
+            type : Number , default: 0, required: true 
+        } ,
+    delverTime :
+    {
+        type: Date, required:true
+    },
+    images: [
+        {
+          url: {
+            type: String,
+            required: true,
+          },
+        },
+      ]
+})
+
