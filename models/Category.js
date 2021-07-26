33427db4581175e2f7aa4fd7be769752f6d3572f
Shakhriyor11 <shaxrik113099@gmail.com>
2021-07-26 16:58:25 +0500

    aa

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,13 @@
-/*
- name:{
-     uz:{type:String, required:true},
-     ru:{type:String, required:true},
- }
-*/
+const mongoose = require('mongoose');
+const categorySchema = new mongoose.Schema({
+    name:{
+        uz:{type:String, required:true},
+        ru:{type:String, required:true},
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+module.exports = mongoose.model('Category',categorySchema);
--- models/Chegirma.js
+++ models/Chegirma.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
+const chegirmaSchema = mongoose.Schema({
+    amount:{
+        type:String,
+        required:true
+    },
+    productID:{
+        type: mongoose.Schema.ObjectId,
+        ref:'Product',
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
 
-/*
-productID,
-amount
-*/
+module.exports = mongoose.model('Chegirma',chegirmaSchema);
